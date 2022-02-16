4354577650d59ebc6c32b343b8730b1782ec16d0
ANDREY <rodion2rodion@yandex.ru>
2022-02-17 02:01:31 +0300

    add exceptions and other minor fixes

--- client.py
+++ client.py
@@ -1,27 +1,48 @@
 import socket
+from datetime import datetime
+from os import system
 import threading
 
 client = socket.socket(
     socket.AF_INET,
-    socket.SOCK_STREAM #TCP/IP
+    socket.SOCK_STREAM  #TCP/IP
 )
 
-client.connect(
-    ("127.0.0.1", 1234)  # localhost
-)
+def setup_connection():
+    try:
+        client.connect(
+            ("127.0.0.1", 1234)  # localhost
+        )
+
+    # put an exception to show users that server is not ready to work, and they should run server.py first
+    except OSError:
+        print("Sorry, our tiny server is not available\n"
+              "Please, try later")
+        exit(0)
 
 def listen_server():
+    messages = ""
     while True:
-        data = client.recv(2048)
-        print(data.decode("utf-8"))
+        try:
+            data = client.recv(2048)
+            messages += f"{datetime.now().date()}" + ": " + data.decode('utf-8') + "\n"
+            system("cls") # "cls" helps us to avoid duplicate lines
+            print(messages)
 
+        # put an exception to inform users about server's disconnect
+        except ConnectionResetError:
+            print("\n"
+                  "Oops! Server is not available anymore....\n"
+                  "Please, restart server and try again")
+            return
 
 def send_all():
     listen_thread = threading.Thread(target=listen_server)
     listen_thread.start()
 
     while True:
-        client.send(input("Me:").encode("utf-8"))
+        client.send(input("").encode("utf-8"))
 
 if __name__ == '__main__':
+    setup_connection()
     send_all()
--- server.py
+++ server.py
@@ -12,25 +12,33 @@ server.bind(
 
 server.listen(10)  # 10 active connections
 users = []
-print("Server is listening")
-
+print("Server is ready")
 
 def listen_user(user):
     while True:
-        data = user.recv(2048)
-        print(f"User sent {data}")
-        send_all(data)
+        try:
+            message = user.recv(2048)
+            print(f"User sent {message}")
+            send_all(message)
 
+        # put an exception to show that someone left chatroom
+        except ConnectionResetError:
+            users.remove(user)
+            print("Someone left chat")
+            return
 
 def send_all(data):
     for user in users:
         user.send(data)
 
-
 def start_server():
     while True:
         user_socket, address = server.accept()
         print(f"User <{address[0]}> connected")  # get client's IP address
+        user_socket.send("CONNECTED \n"
+                         "===========================================================\n"
+                         "= Welcome to the smallest anonymous chat around Internet! =\n"
+                         "===========================================================\n".encode("utf-8"))
         users.append(user_socket)
         listen_accepted_user = threading.Thread(
             target=listen_user,
@@ -38,7 +46,6 @@ def start_server():
         )
 
         listen_accepted_user.start()
-
 
 if __name__ == '__main__':
     start_server()
